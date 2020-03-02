3ef0bcd29674251c589ecabf153bde259ebd36f3
Krzysztof Durczak <krzysztof.0.durczak@gmail.com>
2020-03-02 22:02:42 +0100

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'application/food_emperor_app.dart';
+
+void main() => runApp(FoodEmperorApp());
+
---
+++ lib/resource/colors.dart
@@ -0,0 +1,5 @@
+class Colors{
+  static final int primaryColor = 0xFFFFFFFF;
+  static final int primaryColorDark = 0xFFDDDDDD;
+  static final int accentColor = 0xFFDAAF06;
+}
