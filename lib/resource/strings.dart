ae26f6dbc001ebe79be3d4887263b9e51ed120ae
Krzysztof Durczak <krzysztof.0.durczak@gmail.com>
2020-03-02 22:27:19 +0100

    Refactor initial project

--- lib/resource/strings.dart
+++ lib/resource/strings.dart
@@ -1,3 +1,6 @@
 class Strings{
   static final String commonWelcome = "Hello world";
+  static final String applicationName = "FoodEmperor";
+
+  static final String mainPageName = "Main Page";
 }
---
+++ lib/ui/main_page/main_page.dart
@@ -0,0 +1,20 @@
+import 'package:first_app/ui/main_page/main_page_state.dart';
+import 'package:flutter/widgets.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  MainPageState createState() => MainPageState();
+}
